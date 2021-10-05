7427d1c37d5b0d43d3d8037e897dcc47e5a6425e
Davuong93 <87043428+Davuong93@users.noreply.github.com>
2021-10-04 23:50:47 -0400

    Spinner added
    Spinner added with ID on option

--- fetch.js
+++ fetch.js
@@ -1,13 +1,16 @@
-const entry = document.getElementById("country");
-const codeSentence = document.getElementById("statement");
+const entry = document.getElementById("country")
+const codeSentence = document.getElementById("statement")
+const loader = document.getElementById("spinner")
 
 //fetch function
 function getCountries() {
+    showSpinner(true);
     fetch("http://127.0.0.1:3000/countries")
         .then((data) => {
             return data.json();
         })
         .then((json) => {
+            showSpinner(false)
             json.countries.forEach((item) => {
                 addCountry(item.name, item.code);
             })
@@ -22,7 +25,15 @@ function addCountry(name, code) {
     var option = document.createElement("option");
     option.text = name;
     option.value = code;
-    entry.appendChild(option);
+    entry.appendChild(option,0);
+}
+//spinner to show/hide
+function showSpinner(isLoading) {
+    if (isLoading == true) {
+        loader.style.visibility = "visible";
+    } else {
+        loader.style.visibility = "hidden";
+    }
 }
 
 //Display Results of the country Code
